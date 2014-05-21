2b7d35cda3a7638e26fe375aa7dd301f9ae3b0b9
anton <AntonAbramov1990@gmail.com>
2014-05-21 21:46:29 +0300

    add layout

--- js/init.js
+++ js/init.js
@@ -1,59 +1,9 @@
 $(document).ready(function () {
 
-	popup();
-	nav();
-	tabs();
-	$(".enter-box > button").on("click", function(event){
-		event.preventDefault();
-		event.stopPropagation();
-		if ($(this).hasClass("enter-button")) {
-			if ($(".enter-form.enter").is(":visible")) {
-				$(".enter-form").hide();
-			}
-			else {
-				$(".enter-form.enter").show();
-				$(".enter-form.registration").hide();
-			}
-
-		}
-		else if($(this).hasClass("registration-button")) {
-			if ($(".enter-form.registration").is(":visible")) {
-				$(".enter-form").hide();
-			}
-			else {
-				$(".enter-form.enter").hide();
-				$(".enter-form.registration").show();
-			}
-
-		}
-	});
-	$(document).on("click", function(event){
-		if ($(event.target).closest(".enter-form").length) {
-			return ;
-		}
-		else {
-			$(".enter-form").hide();
-		}
-
-/*		if ($(event.target).closest(".user-info-popover").length) {
-			return false;
-		}
-		else if ($(event.target).closest(".user-info-js").length) {
-			return false;
-		}
-		else {
-			$(".user-info-popover").fadeOut();
-		}*/
-	});
-
-	/*$(".user-info-js").on("click", function(event){
-		event.preventDefault();
-		$(this).parents(".user-section").find(".user-info-popover").fadeToggle();
-	});*/
 
 });
 
-$(window).resize(function(){
+$(window).resize(function () {
 
 });
 
@@ -62,37 +12,30 @@ $(window).load(function () {
 });
 
 
-var nav = function(){
-	$(".second-sub-menu").find("> a").on("click", function(event) {
-		event.preventDefault();
-		$(this).parent().toggleClass("opened");
-	});
-}
-
-var popup = function() {
-	$(".overlay").on("click", function(){
+var popup = function () {
+	$(".overlay").on("click", function () {
 		$(this).fadeOut();
 		$('.popup').fadeOut();
 	})
-	$(".popup").find(".close").on("click", function(){
+	$(".popup").find(".close").on("click", function () {
 		$(".overlay").fadeOut();
 		$('.popup').fadeOut();
 	});
 
 }
-var tabs = function(){
+var tabs = function () {
 	var tab = $(".tabs");
 	if (tab.length) {
-		tab.each(function(){
+		tab.each(function () {
 			var idx = $(this).find(".tab-nav .active").index();
 			$(this).find('.tab-content .tab').hide().eq(idx).show();
 		});
 
 
-		tab.find(".tab-nav a").click(function(event) {
+		tab.find(".tab-nav a").click(function (event) {
 			event.preventDefault();
 
-			if($(this).hasClass("active")) {
+			if ($(this).hasClass("active")) {
 				return false;
 			} else {
 				$(this).parents('.tabs').find(".tab-nav a").removeClass('active');

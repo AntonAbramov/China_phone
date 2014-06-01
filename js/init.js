f44b129d6a15ed273ba26aa581c37f9ae0d589e1
anton <AntonAbramov1990@gmail.com>
2014-06-01 20:41:11 +0300

    add atalog page

--- js/init.js
+++ js/init.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   cart();
   slider();
   catalogBtn();
-
-
+  filterSection();
+  triggerProducts();
 
 });
 
@@ -17,76 +17,76 @@ $(window).load(function () {
 
 
 var popup = function () {
-	$(".overlay").on("click", function () {
+	$('.overlay').on('click', function () {
 		$(this).fadeOut();
 		$('.popup').fadeOut();
 	})
-	$(".popup").find(".close").on("click", function () {
-		$(".overlay").fadeOut();
+	$('.popup').find('.close').on('click', function () {
+		$('.overlay').fadeOut();
 		$('.popup').fadeOut();
 	});
 
 }
 var tabs = function () {
-	var tab = $(".tabs");
+	var tab = $('.tabs');
 	if (tab.length) {
 		tab.each(function () {
-			var idx = $(this).find(".tab-nav .active").index();
+			var idx = $(this).find('.tab-nav .active').index();
 			$(this).find('.tab-content .tab').hide().eq(idx).show();
 		});
 
 
-		tab.find(".tab-nav a").click(function (event) {
+		tab.find('.tab-nav a').click(function (event) {
 			event.preventDefault();
 
-			if ($(this).hasClass("active")) {
+			if ($(this).hasClass('active')) {
 				return false;
 			} else {
-				$(this).parents('.tabs').find(".tab-nav a").removeClass('active');
-				var idx = $(this).addClass("active").index();
-				$(this).parents('.tabs').find(".tab-content .tab").hide();
-				$(this).parents('.tabs').find(".tab-content .tab").eq(idx).show();
+				$(this).parents('.tabs').find('.tab-nav a').removeClass('active');
+				var idx = $(this).addClass('active').index();
+				$(this).parents('.tabs').find('.tab-content .tab').hide();
+				$(this).parents('.tabs').find('.tab-content .tab').eq(idx).show();
 			}
 		});
 	}
 }
 
 var cart = function () {
-  if ($(".cart-entity").length) {
+  if ($('.cart-entity').length) {
 
-    $(".cart-btn").on("click", function() {
-      $('.cart').toggleClass("show");
+    $('.cart-btn').on('click', function() {
+      $('.cart').toggleClass('show');
       setTimeout(function(){
         $('.cart').toggleClass('cart-animation');
       })
     });
 
     $(document).click(function (event) {
-      if ($(event.target).closest(".cart-entity").length) {
+      if ($(event.target).closest('.cart-entity').length) {
         event.stopPropagation();
         return false
       }
-      else if ($(event.target).closest(".cart-btn").length) {
+      else if ($(event.target).closest('.cart-btn').length) {
       event.stopPropagation();
         return false
       }
       else {
-        if ($(".cart").hasClass("show")) {
-          $(".cart").removeClass("show").removeClass("cart-animation");
+        if ($('.cart').hasClass('show')) {
+          $('.cart').removeClass('show').removeClass('cart-animation');
         }
       }
     });
 
-    $(".cart .close").on("click", function(event) {
+    $('.cart .close').on('click', function(event) {
       event.stopPropagation();
-      $(this).parents("tr").next().remove();
-      $(this).parents("tr").remove();
+      $(this).parents('tr').next().remove();
+      $(this).parents('tr').remove();
     })
   }
 }
 var slider = function() {
-  if ($(".slider").length) {
-    $(".slider").slidesjs({
+  if ($('.slider').length) {
+    $('.slider').slidesjs({
   //	width: 940,
       height: 263
     });
@@ -94,12 +94,35 @@ var slider = function() {
 }
 
 var catalogBtn = function() {
-  var catalog = $(".catalog");
-  catalog.find("button.orange").on("click", function(){
+  var catalog = $('.catalog');
+  catalog.find('button.orange').on('click', function(){
 
-    catalog.toggleClass("catalog-show");
+    catalog.toggleClass('catalog-show');
     setTimeout(function(){
       catalog.toggleClass('catalog-animation');
     }, 1);
   });
+}
+
+var filterSection = function() {
+  $('.js-filter').on('click', function() {
+    $(this).parents('.filter').toggleClass('opened').find('.inn').slideToggle();
+
+  });
+}
+
+var triggerProducts = function() {
+  if ($('#list-block').is(':checked')) {
+    $('.products').addClass('list-items');
+  }
+  $('.js-trigger-products').find('input').on('change', function(){
+    if ($('#list-block').is(':checked')) {
+      $('.products').addClass('list-items');
+    }
+    else {
+      $('.products').removeClass('list-items');
+    }
+  });
+
+  
 }

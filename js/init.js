c1c026ca348e4833f6ce79cfbef04f643842656d
anton <AntonAbramov1990@gmail.com>
2014-06-22 01:51:20 +0300

    tovar page and basket pages

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-  cart();
-  slider();
-  catalogBtn();
-  filterSection();
-  triggerProducts();
-
+	cart();
+	slider();
+	catalogBtn();
+	filterSection();
+	triggerProducts();
+	carusel();
+	toggleShowMobile();
+	scrollTopFunction();
 });
 
 $(window).resize(function () {
@@ -12,7 +14,7 @@ $(window).resize(function () {
 });
 
 $(window).load(function () {
-
+	tovarPreview();
 });
 
 
@@ -52,82 +54,153 @@ var tabs = function () {
 }
 
 var cart = function () {
-  if ($('.cart-entity').length) {
+	if ($('.cart-entity').length) {
 
-    $('.cart-btn').on('click', function() {
-      $('.cart').toggleClass('show');
-      setTimeout(function(){
-        $('.cart').toggleClass('cart-animation');
-      })
-    });
+		$('.cart-btn').on('click', function () {
+			$('.cart').toggleClass('show');
+			setTimeout(function () {
+				$('.cart').toggleClass('cart-animation');
+			})
+		});
 
-    $(document).click(function (event) {
-      if ($(event.target).closest('.cart-entity').length) {
-        event.stopPropagation();
-        return false
-      }
-      else if ($(event.target).closest('.cart-btn').length) {
-      event.stopPropagation();
-        return false
-      }
-      else {
-        if ($('.cart').hasClass('show')) {
-          $('.cart').removeClass('show').removeClass('cart-animation');
-        }
-      }
-    });
+		$(document).click(function (event) {
+			if ($(event.target).closest('.cart-entity').length) {
+				event.stopPropagation();
+				return false
+			}
+			else if ($(event.target).closest('.cart-btn').length) {
+				event.stopPropagation();
+				return false
+			}
+			else {
+				if ($('.cart').hasClass('show')) {
+					$('.cart').removeClass('show').removeClass('cart-animation');
+				}
+			}
+		});
 
-    $('.cart .close').on('click', function(event) {
-      event.stopPropagation();
-      $(this).parents('tr').next().remove();
-      $(this).parents('tr').remove();
-    })
-  }
+		$('.cart .close').on('click', function (event) {
+			event.stopPropagation();
+			$(this).parents('tr').next().remove();
+			$(this).parents('tr').remove();
+		})
+	}
 }
-var slider = function() {
-  if ($('.slider').length) {
-    $('.slider').slidesjs({
-  //	width: 940,
-      height: 263
-    });
-  }
-}
-
-var catalogBtn = function() {
-  var catalog = $('.catalog');
-  catalog.find('button.orange').on('click', function(){
-
-    catalog.toggleClass('catalog-show');
-    setTimeout(function(){
-      catalog.toggleClass('catalog-animation');
-    }, 1);
-  });
-
-  $('.header-mobile .catalog-menu').find('>li > a').on('click', function(event){
-    event.preventDefault();
-    $(this).parent().toggleClass("active")
-  });
+var slider = function () {
+	if ($('.slider').length) {
+		$('.slider').slidesjs({
+			//	width: 940,
+			height: 263
+		});
+	}
 }
 
-var filterSection = function() {
-  $('.js-filter').on('click', function() {
-    $(this).parents('.filter').toggleClass('opened').find('.inn').slideToggle();
+var catalogBtn = function () {
+	var catalog = $('.catalog');
+	catalog.find('button.orange').on('click', function () {
 
-  });
+		catalog.toggleClass('catalog-show');
+		setTimeout(function () {
+			catalog.toggleClass('catalog-animation');
+		}, 1);
+	});
+
+	$('.header-mobile .catalog-menu').find('>li > a').on('click', function (event) {
+		event.preventDefault();
+		$(this).parent().toggleClass("active")
+	});
 }
 
-var triggerProducts = function() {
-  if ($('#list-block').is(':checked')) {
-    $('.products').addClass('list-items');
-  }
-  $('.js-trigger-products').find('input').on('change', function(){
-    if ($('#list-block').is(':checked')) {
-      $('.products').addClass('list-items');
-    }
-    else {
-      $('.products').removeClass('list-items');
-    }
-  });
+var filterSection = function () {
+	$('.js-filter').on('click', function () {
+		$(this).parents('.filter').toggleClass('opened').find('.inn').slideToggle();
 
-  
+	});
+}
+
+var triggerProducts = function () {
+	if ($('#list-block').is(':checked')) {
+		$('.products').addClass('list-items');
+	}
+	$('.js-trigger-products').find('input').on('change', function () {
+		if ($('#list-block').is(':checked')) {
+			$('.products').addClass('list-items');
+		}
+		else {
+			$('.products').removeClass('list-items');
+		}
+	});
+
+}
+
+tovarPreview = function () {
+	if ($('.tovar-big-preview').length) {
+		setTimeout(function () {
+			$('.tovar-container').find('.tovar-preview').find('ul li').first().find('a').click();
+		}, 10)
+	}
+	$('.tovar-container').find('.tovar-preview').find('a').on('click', function (event) {
+		event.preventDefault();
+		var src;
+		if (!$(this).hasClass('active')) {
+			$('.tovar-container').find('.tovar-preview').find('a').removeClass('active');
+			src = $(this).addClass('active').attr('href');
+			$(this).parents('.tovar-container').find('.big-img').find('img').attr('src', src);
+		}
+	});
+}
+
+carusel = function () {
+	if ($(".tovar-carusel").length) {
+		if ($(window).width() >= 768) {
+			$(".tovar-carusel").jCarouselLite({
+				btnNext: ".next",
+				btnPrev: ".prev",
+				circular: false,
+				scroll: 1,
+				visible: 3
+			});
+		}
+
+	}
+}
+
+toggleShowMobile = function () {
+	if ($(window).width() < 768) {
+		var bool = true;
+
+		$('.description-block-title').on('click', function (e) {
+			$(this).parent().toggleClass('open');
+
+			if (e.target.className == 'carusel-mobile' && bool) {
+				setTimeout(function () {
+					$(".tovar-carusel").jCarouselLite({
+						btnNext: ".next",
+						btnPrev: ".prev",
+						circular: false,
+						scroll: 1,
+						visible: 2
+					}, 1000);
+				});
+				bool = false;
+			}
+		})
+	}
+}
+
+scrollTopFunction = function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.icons-up').fadeIn();
+		} else {
+			$('.icons-up').fadeOut();
+		}
+	});
+
+	$('.icons-up').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
 }
